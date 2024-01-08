47a8d77385b9fa696c12751bd6d3cf3cf41f7c1b
alfkoblischke <alf@koblischke.com>
2024-01-08 17:36:54 +0100

    Update MMM-Traffic.js

--- MMM-Traffic.js
+++ MMM-Traffic.js
@@ -29,8 +29,8 @@ Module.register("MMM-Traffic", {
         this.myDestination = data.resourceSets[0]["resources"][0].routeLegs[0]["endLocation"]["address"].formattedAddress;          
         this.myDistance = data.resourceSets[0]["resources"][0]["travelDistance"];
         this.myDistanceUnit = data.resourceSets[0]["resources"][0]["distanceUnit"];
-        this.myTravelDuration = Math.floor(data.resourceSets[0]["resources"][0]["travelDuration"]) / 60 + " Min.";
-        this.myTravelDurationTraffic = data.resourceSets[0]["resources"][0]["travelDurationTraffic"];                
+        this.myTravelDuration = Math.floor(data.resourceSets[0]["resources"][0]["travelDuration"] /60 ) + " Min.";
+        this.myTravelDurationTraffic = Math.floor(data.resourceSets[0]["resources"][0]["travelDurationTraffic"] / 60) + " Min.";                
         this.loaded = true;
         this.updateDom();
       }
