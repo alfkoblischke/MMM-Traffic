ccaf6cb43f1e905dc6fd278e7eec20118e7cb6c6
alfkoblischke <alf@koblischke.com>
2024-01-08 15:46:00 +0100

    Create MMM-Traffic.js

---
+++ MMM-Traffic.js
@@ -0,0 +1,109 @@
+Module.register("MMM-Traffic", {
+  defaults: {    
+    updateInterval: 600000,
+    ApiKey: {
+      "a6ee8177-107b-47dd-bcfd-30960ccc6e9c": "Köln"      
+    },    
+  },
+
+  getStyles: function () {
+    return ["style.css"];
+  },  
+
+  // Override start method
+  start: function () {
+    this.loaded = false;    
+    this.url = `https://pegelonline.wsv.de/webservices/rest-api/v2/stations/${Object.keys(this.config.pegelName)}/W/measurements.json`;  
+    this.getData();
+    setInterval(() => {
+      this.getData();      
+    }, this.config.updateInterval);
+  },
+
+  getData: async function () {
+    try {
+      const response = await fetch(this.url);
+      const data = await response.json();            
+      this.letzterPegel1 = data[data.length-1]['value'];     
+      this.loaded = true;
+      this.updateDom();
+    } catch (error) {
+      Log.error(`Fehler beim Abrufen der Daten von Pegel API: ${error}`);
+    }
+    try {
+      const stationResponse = await fetch(this.stationurl);
+      const data = await stationResponse.json();            
+      this.stationName = data['longname'];
+      this.stationName = this.stationName.toLowerCase();      
+      this.loaded = true;
+      this.updateDom();
+    } catch (error) {
+      Log.error(`Fehler beim Abrufen der Daten von Bing API: ${error}`);
+    }    
+  },
+  
+  getHeader: function () {
+    return "Meine Strecke";
+  },
+
+  getDom: function () {
+    var wrapper = document.createElement("table");
+    wrapper.className = "small traffic-table";
+
+    if (!this.loaded) {
+      wrapper.innerHTML = "Lade aktuelle Verkehrslage...";
+      wrapper.className = "dimmed light";
+      return wrapper;
+    }
+
+    // Header Row
+    var headerRow = document.createElement("tr");
+    var stationNameHeader = document.createElement("th");
+    stationNameHeader.innerHTML = "Ort";
+    headerRow.appendChild(stationNameHeader);
+    var waterNameHeader = document.createElement("th");
+    waterNameHeader.innerHTML = "Fluss";
+    headerRow.appendChild(waterNameHeader);
+    var kmHeader = document.createElement("th");
+    kmHeader.innerHTML = "Km";
+    headerRow.appendChild(kmHeader);
+    var timeHeader = document.createElement("th");
+    timeHeader.innerHTML = "Uhrzeit";
+    headerRow.appendChild(timeHeader);
+    var pegelHeader = document.createElement("th");
+    pegelHeader.innerHTML = "Pegel";
+    headerRow.appendChild(pegelHeader);
+    wrapper.appendChild(headerRow);
+
+    // 1 Data Row
+    var row = document.createElement("tr");
+
+    // Pegel Name
+    var pegelName = document.createElement("td");
+    pegelName.innerHTML = this.stationName;
+    row.appendChild(pegelName);
+
+    // Water Name
+    var waterName = document.createElement("td");
+    waterName.innerHTML = this.stationWater;    
+    row.appendChild(waterName);
+
+     // Pegel Km
+    var pegelKm = document.createElement("td");
+    pegelKm.innerHTML = this.stationKm;
+    row.appendChild(pegelKm);
+
+    // Pegel Time
+    var pegelTime = document.createElement("td");
+    pegelTime.innerHTML = this.letzterPegelTime1;
+    row.appendChild(pegelTime);
+    
+    // Pegel Height
+    var pegelHeight = document.createElement("td");
+    pegelHeight.innerHTML = this.letzterPegel1 + " cm";
+    row.appendChild(pegelHeight);
+
+    wrapper.appendChild(row);    
+    return wrapper;
+  }
+});
